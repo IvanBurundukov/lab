626d6760e0b0cb0b303456d1f08ec1b121d03b04
IvanBurundukov <46565393+IvanBurundukov@users.noreply.github.com>
2019-01-10 17:22:11 +0300

    new
    new ok

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, IVAN");
+            Console.WriteLine("Hello, IVANBurundukov");
             Console.WriteLine("New string");
         }
     }
